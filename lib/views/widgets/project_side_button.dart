937465f249314ae8549e38e30b966d23f134c7b9
Belot-Sarrailh Valentin <valentin.belot-sarrailh@etudiant.univ-lr.fr>
2022-01-06 15:07:17 +0100

    saveProjectSideButton

--- lib/views/widgets/project_side_button.dart
+++ lib/views/widgets/project_side_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:renov_proprietaire_app/values/colors.dart';
+import 'package:renov_proprietaire_app/views/widgets/icon_background_circle.dart';
 import 'package:renov_proprietaire_app/views/widgets/page_title.dart';
 
 
@@ -13,7 +14,7 @@ class ProjectSideButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       child: Row(children: [
-        PageTitle(text: "Bonjour"),
+        const IconBackgroundCircle(imageSize: 100, topCirclePosition: 30, rightCirclePosition: 30, url: "icons/house.svg"),
         Text(text),
       ]),
       style: ElevatedButton.styleFrom(
