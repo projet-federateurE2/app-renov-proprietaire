8b12354dda3c4255d2a6c9fe756297a1888399c9
Virieux-Petit Lucas <lucas.virieuxpetit@gmail.com>
2022-01-06 15:07:53 +0100

    feat/ work type clickable block file created

---
+++ lib/views/widgets/work_type_clickable_block.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:renov_proprietaire_app/values/colors.dart';
+
+class WorkTypeClickableBlock extends StatelessWidget {
+  final Widget iconBackgroundCircle;
+  final String workType;
+  final String WorkTypeDescription;
+
+  const WorkTypeClickableBlock(
+      {Key? key,
+        required this.iconBackgroundCircle,
+        required this.workType,
+        required this.WorkTypeDescription,})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+
+    );
+  }
+}
