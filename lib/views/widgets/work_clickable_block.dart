594d965d722a17395bfcbe320bb46aa9316057b2
RachelCauchois <rachel.cauchois@etudiants.univ-lr.fr>
2022-01-06 14:27:01 +0100

    feat - new clickable Box for works

---
+++ lib/views/widgets/work_clickable_block.dart
@@ -0,0 +1,17 @@
+import 'package:renov_proprietaire_app/values/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WorkClickableBlock extends StatefulWidget {
+  const WorkClickableBlock({Key? key}) : super(key: key);
+
+  @override
+  State<WorkClickableBlock> createState() => _WorkClickableBlockState();
+}
+
+class _WorkClickableBlockState extends State<WorkClickableBlock> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
