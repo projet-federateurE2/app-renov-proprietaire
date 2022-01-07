9f916b08f646350ae61821e3447fd21ababe5626
Belot-Sarrailh Valentin <valentin.belot-sarrailh@etudiant.univ-lr.fr>
2022-01-07 10:21:21 +0100

    saveProject

--- lib/views/widgets/project_side_button.dart
+++ lib/views/widgets/project_side_button.dart
@@ -13,17 +13,27 @@ class ProjectSideButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      child: Row(children: [
-        const IconBackgroundCircle(imageSize: 100, topCirclePosition: 30, rightCirclePosition: 30, url: "icons/house.svg"),
-        Text(text),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child : const IconBackgroundCircle(imageSize: 80, topCirclePosition: 0, rightCirclePosition:5, url: "icons/house.svg"),
+          ),
+
+        Text(text, style: const TextStyle(color: ColorsRenov.darkBlue, fontSize: 14, fontWeight: FontWeight.w600)),
       ]),
       style: ElevatedButton.styleFrom(
         elevation: 0.0,
         shadowColor: Colors.black,
-        primary: ColorsRenov.white,
+        primary: Colors.white,
+        fixedSize: Size(400, 70),
+
         padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(28)),
-      ),
+      ), 
+      
+      
       onPressed: () {},
     );
   }
