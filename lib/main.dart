1ccedb131956c0e7b9735da0cb5932ba5b69cc12
Nathan Bregaint <nathan.bregaint@etudiant.univ-lr.fr>
2022-01-07 12:08:19 +0100

    feat/work_type_selection_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:renov_proprietaire_app/views/pages/work_type_selection_page.dart';
 import 'package:renov_proprietaire_app/views/widgets/background_green_wave.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'App renov',
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: const MyHomePage(),
+      home: const WorkTypeSelectionPage(),
     );
   }
 }
