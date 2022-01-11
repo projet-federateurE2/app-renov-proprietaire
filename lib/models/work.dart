1ef89c9e817149b0fca5c3c8163f064f2f088e5b
Belot-Sarrailh Valentin <valentin.belot-sarrailh@etudiant.univ-lr.fr>
2022-01-11 14:38:50 +0100

    creation class Work

---
+++ lib/models/work.dart
@@ -0,0 +1,12 @@
+import 'package:renov_proprietaire_app/views/widgets/icon_background_circle.dart';
+
+class Travaux {
+  late String fullTitle;
+  late String littleTitle;
+  late IconBackgroundCircle image;
+  late String help;
+  late String alert;
+  late String type;
+
+  Travaux(this.fullTitle, this.littleTitle, this.type, this.image, this.help, this.alert);
+}
