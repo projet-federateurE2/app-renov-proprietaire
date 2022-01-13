d13be7140e2cb43cb37424be9397f794e66dd5ed
RachelCauchois <rachel.cauchois@etudiants.univ-lr.fr>
2022-01-13 11:47:42 +0100

    Work Selection Page in progress, beginning of relations between widgets

--- lib/views/pages/work_selection_page.dart
+++ lib/views/pages/work_selection_page.dart
@@ -2,38 +2,99 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:renov_proprietaire_app/views/widgets/background_green_wave.dart';
 import 'package:renov_proprietaire_app/views/widgets/page_title.dart';
+import 'package:renov_proprietaire_app/views/widgets/work_clickable_block.dart';
+import 'package:renov_proprietaire_app/views/widgets/work_selection_empty_description.dart';
 
 class WorkSelectionPage extends StatefulWidget {
   final String workType;
-  
-  const WorkSelectionPage({ Key? key, required this.workType }) : super(key: key);
+
+  const WorkSelectionPage({Key? key, required this.workType}) : super(key: key);
 
   @override
   _WorkSelectionPageState createState() => _WorkSelectionPageState();
 }
 
 class _WorkSelectionPageState extends State<WorkSelectionPage> {
+  var workname1 = "hihi";
+
+  callback(newAbc) {
+    setState(() {
+      workname1 = newAbc;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
+        backgroundColor: Colors.white,
+        body: Stack(children: [
           const BackgroundGreenWave(),
           Container(
             padding: const EdgeInsets.all(20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Hero(tag: "title-select-work", child: Material(
-                  type: MaterialType.transparency, // likely needed
-                  child: PageTitle(text: "Travaux d'isolation"))),
-                Row()
+                Hero(
+                    tag: "title-select-work",
+                    child: Material(
+                        type: MaterialType.transparency, // likely needed
+                        child: PageTitle(text: "Travaux d'isolation"))),
+                SingleChildScrollView(
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                          flex: 3,
+                          child: GridView.count(
+                              mainAxisSpacing: 10.0,
+                              crossAxisSpacing: 10.0,
+                              scrollDirection: Axis.vertical,
+                              crossAxisCount: 2,
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              children: [
+                                WorkClickableBlock(
+                                  urlImage: './../../../assets/icons/floor.svg',
+                                  workName: workname1,
+                                  callback: callback,
+                                ),
+                                WorkClickableBlock(
+                                    urlImage:
+                                        './../../../assets/icons/floor.svg',
+                                    workName: 'ffeffffe',
+                                    callback: callback),
+                                WorkClickableBlock(
+                                    urlImage:
+                                        './../../../assets/icons/floor.svg',
+                                    workName: 'ffeffffe',
+                                    callback: callback),
+                                WorkClickableBlock(
+                                    urlImage:
+                                        './../../../assets/icons/floor.svg',
+                                    workName: 'ffeffffe',
+                                    callback: callback),
+                                WorkClickableBlock(
+                                    urlImage:
+                                        './../../../assets/icons/floor.svg',
+                                    workName: 'ffeffffe',
+                                    callback: callback),
+                                WorkClickableBlock(
+                                    urlImage:
+                                        './../../../assets/icons/floor.svg',
+                                    workName: 'ffeffffe',
+                                    callback: callback)
+                              ])),
+                      Expanded(
+                        flex: 7,
+                        child: Column(
+                          children: const [WorkSelectionEmptyDescription()],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           )
-        ]
-      )
-    );
+        ]));
   }
 }
--- lib/views/widgets/work_clickable_block.dart
+++ lib/views/widgets/work_clickable_block.dart
@@ -4,54 +4,61 @@ import 'package:renov_proprietaire_app/values/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
 class WorkClickableBlock extends StatefulWidget {
   final String urlImage;
   final String workName;
-  const WorkClickableBlock({Key? key, required this.urlImage, required this.workName}) : super(key: key);
+  final Function(String) callback;
+
+  const WorkClickableBlock(
+      {Key? key,
+      required this.urlImage,
+      required this.workName,
+      required this.callback})
+      : super(key: key);
 
   @override
   State<WorkClickableBlock> createState() => _WorkClickableBlockState();
 }
+
 class _WorkClickableBlockState extends State<WorkClickableBlock> {
   var backgroundColor = Colors.white;
   var fontColor = ColorsRenov.primaryGreen;
-  @override
-    Widget build(BuildContext context) {
-      return SizedBox(
-          width: 183.0,
-          height: 183.0,
-          child: Card(
-              elevation: 4.0,
-              color: backgroundColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: InkWell(
-                  onTap: () {
-                    setState(() {
-                      if(backgroundColor == Colors.white){
-                        backgroundColor = ColorsRenov.primaryGreen;
-                        fontColor = Colors.white;
-                      } else {
-                        backgroundColor = Colors.white;
-                        fontColor = ColorsRenov.primaryGreen;
-                      }
-                    });
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                          width:90,
-                          height:90,
-                          child: SvgPicture.asset(widget.urlImage, color: fontColor)
-                      ),
-                      Text(widget.workName,
-                          style: TextStyle(color: fontColor, fontSize: 20, fontWeight: FontWeight.w500)
-                      ),
-                    ],))
-              )
 
-      );
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 4.0,
+        color: backgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: InkWell(
+            onTap: () {
+              setState(() {
+                if (backgroundColor == Colors.white) {
+                  widget.callback("RANDON TEXT");
+                  backgroundColor = ColorsRenov.primaryGreen;
+                  fontColor = Colors.white;
+                } else {
+                  backgroundColor = Colors.white;
+                  fontColor = ColorsRenov.primaryGreen;
+                }
+              });
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: SvgPicture.asset(widget.urlImage, color: fontColor)),
+                Text(widget.workName,
+                    style: TextStyle(
+                        color: fontColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500)),
+              ],
+            )));
   }
 }
