6e4e9bc0a1bb3c5b1ea9c635e4019df88ab3a9b4
PillouMK <maxence.pille@gmail.com>
2022-02-04 10:35:48 +0100

    indentation fixed

--- lib/views/widgets/work_selection_empty_description.dart
+++ lib/views/widgets/work_selection_empty_description.dart
@@ -11,42 +11,50 @@ class WorkSelectionEmptyDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var iconBackgroundCircle = IconBackgroundCircle(
-        imageSize: MediaQuery.of(context).size.width >
-                MediaQuery.of(context).size.height
-            ? MediaQuery.of(context).size.width * 0.12
-            : MediaQuery.of(context).size.height * 0.12,
-        topCirclePosition: 10,
-        rightCirclePosition: 10,
-        url: "icons/house.svg");
+      imageSize:
+          MediaQuery.of(context).size.width > MediaQuery.of(context).size.height
+              ? MediaQuery.of(context).size.width * 0.12
+              : MediaQuery.of(context).size.height * 0.12,
+      topCirclePosition: 10,
+      rightCirclePosition: 10,
+      url: "icons/house.svg",
+    );
 
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.40,
       height: MediaQuery.of(context).size.height * 0.52,
       child: Card(
         elevation: 4,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-                margin: const EdgeInsets.only(
-                  top: 20,
-                  bottom: 4,
-                  left: 10,
-                ),
-                child: iconBackgroundCircle),
+              margin: const EdgeInsets.only(
+                top: 20,
+                bottom: 4,
+                left: 10,
+              ),
+              child: iconBackgroundCircle,
+            ),
             Expanded(
               child: Container(
-                  margin: const EdgeInsets.only(
-                      right: 20, left: 20, top: 16, bottom: 8),
-                  child: const AutoSizeText(
-                    TextRenov.workProjectEmpty,
-                    // textAlign: TextAlign.center,
-                    maxLines: 5,
-                    style:
-                        TextStyle(fontWeight: FontWeight.w300, fontSize: 17.0),
-                  )),
-            )
+                margin: const EdgeInsets.only(
+                  right: 20,
+                  left: 20,
+                  top: 16,
+                  bottom: 8,
+                ),
+                child: const AutoSizeText(
+                  TextRenov.workProjectEmpty,
+                  // textAlign: TextAlign.center,
+                  maxLines: 5,
+                  style: TextStyle(fontWeight: FontWeight.w300, fontSize: 17.0),
+                ),
+              ),
+            ),
           ],
         ),
       ),
