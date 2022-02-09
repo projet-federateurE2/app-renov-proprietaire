de096da44a2c56c432e4f4014268de5af5e89aed
PillouMK <maxence.pille@gmail.com>
2022-02-09 11:56:46 +0100

    fix strings values for text and icons

--- lib/views/widgets/left_side_bar.dart
+++ lib/views/widgets/left_side_bar.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:renov_proprietaire_app/values/colors.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:renov_proprietaire_app/values/strings.dart';
 import 'package:renov_proprietaire_app/values/styles.dart';
 import 'package:renov_proprietaire_app/views/widgets/project_side_button.dart';
@@ -21,65 +19,74 @@ class _LeftSideBarState extends State<LeftSideBar> {
   @override
   Widget build(BuildContext context) {
     return FractionallySizedBox(
-        widthFactor: 0.22,
-        heightFactor: 0.9,
-          child: Column(children: [
-            Expanded(
-                child: Card(
-                    elevation: 4.0,
-                    color: backgroundColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(styleRenov.roundedBorder),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(15.0, 30.0, 15.0, 30.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            Column(children: <Widget>[
-                              SvgPicture.asset('./../../assets/icons/account_avatar.svg', width: 100.0, height: 100.0),
-                              const Text("Paul Dupont",style: TextStyle(
-                                fontWeight: FontWeight.w600, fontSize: 16.0))]),
-                            Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(styleRenov.roundedBorder),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 20.0,horizontal:15.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: const [
-                                    Padding(padding: EdgeInsets.only(left:6.0),
-                                    child: Text(TextRenov.sideProject, style: TextStyle(
-                                        fontWeight: FontWeight.w600, fontSize: 16.0)),),
-                                    Padding(padding: EdgeInsets.only(top:6.0)),
-                                    SecondaryProjectSideButton(
-                                        text: 'Isolation des fenêtres',
-                                        secondaryText: 'vérifier la conformité du devis'),
-                                    Padding(padding: EdgeInsets.only(top:6.0)),
-                                    SecondaryProjectSideButton(
-                                        text: 'Installation pompe à chaleur',
-                                        secondaryText: 'Contacter les artisans')
-                                  ],
+      widthFactor: 0.22,
+      heightFactor: 0.9,
+      child: Column(children: [
+        Expanded(
+            child: Card(
+                elevation: 4.0,
+                color: backgroundColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(styleRenov.roundedBorder),
+                ),
+                child: Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 30.0, 15.0, 30.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Column(children: <Widget>[
+                          SvgPicture.asset(IconsRenov.accountAvatar,
+                              width: 100.0, height: 100.0),
+                          const Text("Paul Dupont",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 16.0))
+                        ]),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(styleRenov.roundedBorder),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 15.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 6.0),
+                                  child: Text(TextRenov.sideProject,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 16.0)),
                                 ),
-                              ),),
-                            const Flexible(
-                                child: ProjectSideButton(
-                                    text: TextRenov.sideProjectBtn1,
-                                    urlIcon: './../assets/icons/profile.svg')),
-                            const Flexible(
-                                child: ProjectSideButton(
-                                    text: TextRenov.sideProjectBtn2,
-                                    urlIcon: './../assets/icons/notepad.svg')),
-                            const Flexible(
-                                child: ProjectSideButton(
-                                    text: TextRenov.sideProjectBtn3,
-                                    urlIcon: './../assets/icons/phone.svg')),
-                          ],
-                        )
-                    )
-))
-          ]),
+                                Padding(padding: EdgeInsets.only(top: 6.0)),
+                                SecondaryProjectSideButton(
+                                    text: 'Isolation des fenêtres',
+                                    secondaryText:
+                                        'vérifier la conformité du devis'),
+                                Padding(padding: EdgeInsets.only(top: 6.0)),
+                                SecondaryProjectSideButton(
+                                    text: 'Installation pompe à chaleur',
+                                    secondaryText: 'Contacter les artisans')
+                              ],
+                            ),
+                          ),
+                        ),
+                        const Flexible(
+                            child: ProjectSideButton(
+                                text: TextRenov.sideProjectBtn1,
+                                urlIcon: IconsRenov.profile)),
+                        const Flexible(
+                            child: ProjectSideButton(
+                                text: TextRenov.sideProjectBtn2,
+                                urlIcon: IconsRenov.notepad)),
+                        const Flexible(
+                            child: ProjectSideButton(
+                                text: TextRenov.sideProjectBtn3,
+                                urlIcon: IconsRenov.phone)),
+                      ],
+                    ))))
+      ]),
     );
   }
 }
--- lib/views/widgets/secondary_project_side_button.dart
+++ lib/views/widgets/secondary_project_side_button.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:renov_proprietaire_app/values/strings.dart';
 
 class SecondaryProjectSideButton extends StatelessWidget {
   final String text;
@@ -13,36 +13,36 @@ class SecondaryProjectSideButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 4,
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.5)),
-        child: InkWell(
-            onTap: () {},
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        text,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 10.0),
-                      ),
-                      Text(
-                        secondaryText,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w400, fontSize: 9.0),
-                      ),
-                    ],
-                  ),
-                  SvgPicture.asset('./../../assets/icons/location.svg')
-                ],
-              ),
-            )),
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.5)),
+      child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      text,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 10.0),
+                    ),
+                    Text(
+                      secondaryText,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w400, fontSize: 9.0),
+                    ),
+                  ],
+                ),
+                SvgPicture.asset(IconsRenov.location)
+              ],
+            ),
+          )),
     );
   }
 }
