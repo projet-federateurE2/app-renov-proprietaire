f97f68c8e19e4682da3e4ef1b77160ce0ada4614
Nathan Bregaint <nathan.bregaint@etudiant.univ-lr.fr>
2022-03-07 14:33:07 +0100

    feat(homepage interface)

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:renov_proprietaire_app/views/widgets/background_green_wave.dart';
 import 'package:renov_proprietaire_app/views/widgets/left_side_bar.dart';
+import 'package:renov_proprietaire_app/views/widgets/notepad.dart';
 import 'package:renov_proprietaire_app/views/widgets/page_title.dart';
 import 'package:renov_proprietaire_app/values/strings.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:renov_proprietaire_app/views/widgets/work_in_progress_block.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,7 +11,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var workInProgress = [0, 1, 2, 3];
+    List<dynamic> works = [
+      {
+        'urlIcon': 'icons/window.svg',
+        'workTitle': 'Isolation des fenêtres',
+        'budget': 'Non renseigné',
+        'financialAssistance': 'Non renseigné',
+        'stepInProgress': 'Trouver un artisan dans la liste',
+        'percentageCompleted': 0
+      },
+      {
+        'urlIcon': 'icons/attic.svg',
+        'workTitle': 'Isolation des combles',
+        'budget': 'Non renseigné',
+        'financialAssistance': 'Non renseigné',
+        'stepInProgress': 'Trouver un artisan dans la liste',
+        'percentageCompleted': 20
+      }
+    ];
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -29,40 +46,39 @@ class HomePage extends StatelessWidget {
                       Container(
                         padding: const EdgeInsets.symmetric(horizontal: 60),
                         child: Column(
+                          mainAxisSize: MainAxisSize.min,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             PageTitle(text: TextRenov.workInProgress),
                             const Text(TextRenov.homePageDesc),
-                            CarouselSlider.builder(
-                                options: CarouselOptions(
-                                    enableInfiniteScroll: false,
-                                    aspectRatio: 2.0,
-                                    enlargeCenterPage: false,
-                                    viewportFraction: 1,
-                                    height: 200.0),
-                                itemCount: (workInProgress.length / 2).round(),
-                                itemBuilder: (context, index, realIndex) {
-                                  final int first = index * 2;
-                                  final int? second = index * 2< (workInProgress.length / 2).round() - 1 ? first + 1 : null;
-                                  return Row(
-                                    children: [first, second].map((idx) {
-                                      return idx != null
-                                      ? WorkInProgressBlock(
-                                          urlIcon: "urlIcon",
-                                          workTitle: "workTitle",
-                                          budget: "budget",
-                                          financialAssistance:
-                                              "financialAssistance",
-                                          stepInProgress:
-                                              "stepInProgress")
-                                              : Container();
-                                    }).toList(),
-                                  );
-                                }),   
+                            SizedBox(
+                              height: 200,
+                              child: ListView.builder(
+                                  itemCount: works.length,
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    var _workInProgress = works[index];
+                                    return WorkInProgressBlock(
+                                        urlIcon: _workInProgress['urlIcon'],
+                                        workTitle: _workInProgress['workTitle'],
+                                        budget: _workInProgress['budget'],
+                                        financialAssistance: _workInProgress['financialAssistance'],
+                                        stepInProgress: _workInProgress['stepInProgress'],
+                                        percentageCompleted: _workInProgress['percentageCompleted'],
+                                        );
+                                  }),
+                            )
                           ],
                         ),
                       ),
-                      Container(
+                      const SizedBox(height: 50),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 60),
+                          child: const Notepad(),
+                        ),
                       )
                     ],
                   ),
